7a7de4127022182e449145cfc0b92b47670fd013
liusuifen <2660444092@qq.com>
2020-04-08 13:51:46 +0800

    提交

--- redis-lock/src/main/java/com/example/redislock/RedisLockApplication.java
+++ redis-lock/src/main/java/com/example/redislock/RedisLockApplication.java
@@ -1,7 +1,10 @@
 package com.example.redislock;
 
+import org.redisson.Redisson;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisLockApplication {
@@ -9,5 +12,21 @@ public class RedisLockApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedisLockApplication.class, args);
     }
+
+    @Bean
+    public Redisson redisson() {
+        // 此为单机模式
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://192.168.0.60:6379").setDatabase(0);
+        /*config.useClusterServers()
+                .addNodeAddress("redis://192.168.0.61:8001")
+                .addNodeAddress("redis://192.168.0.62:8002")
+                .addNodeAddress("redis://192.168.0.63:8003")
+                .addNodeAddress("redis://192.168.0.61:8004")
+                .addNodeAddress("redis://192.168.0.62:8005")
+                .addNodeAddress("redis://192.168.0.63:8006");*/
+        return (Redisson) Redisson.create(config);
+    }
+
 
 }
